d5884d12cbea8f776b1906ef5ff9a8efb8520413
i11m20n <alivemess@gmail.com>
2020-09-10 17:10:30 +0800

    first deploy

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,8 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+// export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x7B79caBD75AbAa11D0b1AB2115E3bb876aF60aFa'
 
 // a list of tokens by chain
 type ChainTokenList = {
